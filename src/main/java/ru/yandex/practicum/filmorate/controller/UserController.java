d26bd7f65f6ec0cceb8bb2fa9ad0bedfe7f57ceb
Di-Mark <dima.markin228@yandex.ru>
2024-02-02 23:10:10 +0300

    Upd: логи и форматирование

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
@@ -10,6 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @Getter
 @RestController
 public class UserController {
@@ -18,6 +20,7 @@ public class UserController {
 
     @GetMapping("/users")
     public List<User> findAllUsers() {
+        log.info("Список юзеров успешно получен");
         return List.copyOf(users.values());
     }
 
@@ -30,20 +33,24 @@ public class UserController {
         user.setId(idUser);
         users.put(idUser, user);
         idUser++;
+        log.info("Юзер успешно создан");
         return user;
     }
 
     @PutMapping("/users")
     public User patchUser(@RequestBody User user) throws ValidationException {
         if (user.getEmail() == null || !user.getEmail().contains("@") || user.getEmail().equals("")) {
+            log.info("почта пользователя пустая или неправильного формата");
             throw new ValidationException("почта пользователя пустая или неправильного формата");
         } else users.get(user.getId()).setEmail(user.getEmail());
 
         if (user.getLogin().equals("") || user.getLogin().contains(" ") || user.getLogin() == null) {
+            log.info("Логин пользователя пустой или содержит пробелы");
             throw new ValidationException("Логин пользователя пустой или содержит пробелы");
         } else users.get(user.getId()).setLogin(user.getLogin());
 
         if (user.getBirthday().isAfter(LocalDate.now())) {
+            log.info("День рождение пользователя не может быть в будущем");
             throw new ValidationException("День рождение пользователя не может быть в будущем");
         } else users.get(user.getId()).setBirthday(user.getBirthday());
 
@@ -51,15 +58,19 @@ public class UserController {
             users.get(user.getId()).setName(user.getLogin());
         } else users.get(user.getId()).setName(user.getName());
 
+        log.info("Юзер успешно обновлен");
         return users.get(user.getId());
     }
 
     private void checkUser(User user) throws ValidationException {
         if (user.getEmail() == null || !user.getEmail().contains("@") || user.getEmail().equals("")) {
+            log.info("почта пользователя пустая или неправильного формата");
             throw new ValidationException("почта пользователя пустая или неправильного формата");
         } else if (user.getLogin().equals("") || user.getLogin().contains(" ") || user.getLogin() == null) {
+            log.info("Логин пользователя пустой или содержит пробелы");
             throw new ValidationException("Логин пользователя пустой или содержит пробелы");
         } else if (user.getBirthday().isAfter(LocalDate.now())) {
+            log.info("День рождение пользователя не может быть в будущем");
             throw new ValidationException("День рождение пользователя не может быть в будущем");
         }
     }
