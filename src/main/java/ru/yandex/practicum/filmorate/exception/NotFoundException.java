faaf0f41d8fa86a78297c630cd5d7e24e9be7ab3
Di-Mark <dima.markin228@yandex.ru>
2024-02-09 21:32:59 +0300

    Upd: форматирование

--- src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.exception;
 
 public class NotFoundException extends RuntimeException {
-    public NotFoundException(String massage){
+    public NotFoundException(String massage) {
         super(massage);
     }
 }
